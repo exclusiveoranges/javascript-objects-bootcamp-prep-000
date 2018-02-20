cdb22d238452b97bb593277368270d701659f53c
Ryan O'Toole <otoolerj@gmail.com>
2018-02-20 18:24:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,10 @@ var playlist = {
   artistnames: "song titles" };
 
   function updatePlaylist(obj, key, value) {
-  return Object.assign({}, obj, { key: [value] })
+  return Object.assign({}, obj, { [key]: value })
   }
 
   function removeFromPlaylist(obj, artistnames) {
   delete playlist.artistnames
   return playlist;
   }
-  
